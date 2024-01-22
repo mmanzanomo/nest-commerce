ef0de1c54854794b38b07f2c7bd3fd3dbcd618ed
Miguel Ángel Manzano <mmanzanomo@gmail.com>
2024-01-22 23:47:06 +0100

    Added product module

---
+++ src/products/dto/get-product.dto.ts
@@ -0,0 +1,10 @@
+
+export class GetProductDto {
+    title: string;
+    price: number;
+    description: string;
+    slug: string;
+    stock: number;
+    sizes: string[];
+    gender: string;
+}
